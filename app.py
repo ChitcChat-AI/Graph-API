4c2cc97f04a0ac01679f8be11d476e9a62504301
DorHaimovich12 <Dor.haimovich@stu.shenkar.ac.il>
2024-06-01 17:46:37 +0300

    added colors and requirements

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
-import re
-from datetime import datetime
-
 from flask import Flask
 from flask import request
-from flask_cors import CORS
-
+from flask_cors import CORS, cross_origin
+from flask import jsonify
 
 from graph import Graph
 
 app = Flask(__name__)
-CORS(app, resources={r"*": {"origins": "*"}})
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
 
 
-@app.route("/graph", methods=["POST"])
+@app.route("/graph/", methods=["POST", "OPTIONS"])
+@cross_origin()
 def create_graph():
-    return Graph(request.get_json()).create_graph()
+    if request.method == "OPTIONS":
+        response = jsonify({})
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Methods", "POST")
+        response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+        return response
+    else:
+        return Graph(request.get_json()).create_graph()
